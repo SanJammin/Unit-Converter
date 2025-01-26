89c1ce9ccf9e3ad54dba1ca1505e14d1d2d8bdde
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-01-26 21:28:37 +0000

    Add volume and mass converter

--- index.js
+++ index.js
@@ -8,12 +8,30 @@ const PMASS = document.getElementById("p-mass");
 CONVERTBTN.addEventListener("click", function() {
     const INPUTVAL = Number(NUMBERINPUT.value);
 
-
+    // Length Converter
     let metersToFeet = INPUTVAL * 3.281;
     let feetToMeters = INPUTVAL / 3.281;
 
     PLENGTH.innerHTML = `
         ${INPUTVAL} metres = ${metersToFeet.toFixed(3)} feet | 
-        ${INPUTVAL} feet = ${feetToMeters.toFixed(3)}
+        ${INPUTVAL} feet = ${feetToMeters.toFixed(3)} meters
+    `
+
+    // Volume Converter
+    let litresToGallons = INPUTVAL * 0.264;
+    let gallonsToLitres = INPUTVAL / 0.264;
+
+    PVOLUME.innerHTML = `
+        ${INPUTVAL} litres = ${litresToGallons.toFixed(3)} gallons | 
+        ${INPUTVAL} gallons = ${gallonsToLitres.toFixed(3)} litres
+    `
+
+    // Mass Converter
+    let kilosToPounds = INPUTVAL * 2.204;
+    let poundsToKilos = INPUTVAL / 2.204;
+
+    PMASS.innerHTML = `
+        ${INPUTVAL} kilos = ${kilosToPounds.toFixed(3)} pounds | 
+        ${INPUTVAL} pounds = ${poundsToKilos.toFixed(3)} kilos
     `
 });
